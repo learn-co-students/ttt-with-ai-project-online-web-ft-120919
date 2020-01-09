6cbdfd52d6c276c98f6ffb60e9995d6f4d3d8115
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-09 16:08:16 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,47 +1,48 @@
 class Board
   attr_accessor :cells
-  def initialize
-    @cells = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
-  end
+
+  @cells = []
 
   def reset!
-    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    self.cells = Array.new(9, " ")
+  end
+
+  def initialize
+    self.cells = Array.new(9, " ")
   end
 
   def display
-    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
     puts "-----------"
-    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
     puts "-----------"
-    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
   end
 
   def position(user_input)
-    @cells[user_input.to_i - 1]
+    self.cells[user_input.to_i-1]
   end
 
   def full?
-    @cells.all? {|x| x != " "}
+    self.cells.all? {|cell| cell == "X" || cell == "O"}
   end
 
   def turn_count
-    @cells.collect {|x| x == " "}.size / 3
+    self.cells.count("X") + self.cells.count("O")
   end
 
-  def taken?(position)
-    cells[position.to_i - 1] == "X" || cells[position.to_i - 1] == "O"
+  def taken?(position) #check board position
+    self.cells[position.to_i-1] == "X" || #board position -1 because of test using range 1-9 (user input numbers)
+    self.cells[position.to_i-1] == "O"
   end
 
   def valid_move?(position)
-    !(taken?(position))  && (0..9).include?(position.to_i - 1)
+    !taken?(position) && position.to_i >0 && position.to_i <=9
   end
-  
-  #update updates the cells in the board with the player token according to the input
-  token = "O"
-  
-  def update(position, token)
-    cells[position.to_i - 1] = token
-    display
+
+  def update(position, player)
+      self.cells[position.to_i-1] = player.token
   end
+
 
 end
